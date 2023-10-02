54f027299659a9bd1a876739cad25e0dc6bec8b2
svartse <killbillbeatrixx@gmail.com>
2023-10-02 22:52:24 +0300

    .env

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -2,6 +2,8 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
 
+const { Configuration, OpenAIApi } = require('openai');
+
 dotenv.config();
 
 const router = express.Router();
@@ -10,7 +12,7 @@ const configuraiton = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-const api = new OpenAIApi(configuraiton);
+const openai = new OpenAIApi(configuraiton);
 
 router.route('/').get((req, res) => {
     res.send('Hello from Dall-e');
